c8cdcd533ec9d970618bf614c0b21c314fea06ec
ciwag-creators <ciwag255@gmail.com>
2025-10-31 12:46:24 +0100

    still working on tailwindcss

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,11 @@
-content: ["./src/**/*.{js,jsx,ts,tsx}"],
-theme: { extend: {} },
-plugins: [],
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
