6e60519331e21f325fc0c9fb5918a474e427d6cc
ciwag-creators <ciwag255@gmail.com>
2025-11-06 09:13:49 +0100

    working on register

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,15 +1,12 @@
+// client/src/api/index.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL, // Must end WITHOUT /api
 });
-
 
 // AUTH
 export const registerUser = (data) => API.post("/api/users/register", data);
 export const loginUser = (data) => API.post("/api/users/login", data);
-
-// Example protected request later:
-export const getUserProfile = () => API.get("/api/users/profile");
 
 export default API;
