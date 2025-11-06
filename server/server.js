e3b582a784dd7065a93aeaa3f08fc6c7bd683c27
ciwag-creators <ciwag255@gmail.com>
2025-11-06 18:51:31 +0100

    register4

--- server/server.js
+++ server/server.js
@@ -11,14 +11,17 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://safe-mother.vercel.app",
+  "https://safemother-front.vercel.app"
+];
+
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://safe-mother.vercel.app",
-    "https://safemother-front.vercel.app"
-  ],
+  origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 
 app.use(express.json());
