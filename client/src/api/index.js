00327ad56fff70db4bdcb26241d286a1d2b2fc0b
ciwag-creators <ciwag255@gmail.com>
2025-11-07 10:23:18 +0100

    index.js updated

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
-const API_URL = "https://safemother.onrender.com/api/users";
+const API_URL = "https://safemother.onrender.com/api";
 
-export const registerUser = (data) => axios.post(`${API_URL}/register`, data);
-export const loginUser = (data) => axios.post(`${API_URL}/login`, data);
+const API = {
+  registerUser: (data) => axios.post(`${API_URL}/users/register`, data),
+  loginUser: (data) => axios.post(`${API_URL}/users/login`, data),
+  getTips: () => axios.get(`${API_URL}/tips`),
+  getQuestions: () => axios.get(`${API_URL}/questions`),
+  addReminder: (data) => axios.post(`${API_URL}/reminders`, data),
+};
+
+export default API;
